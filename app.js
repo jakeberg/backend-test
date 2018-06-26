8ee0a25240975a34add5917ebc4fa6c1e750b992
Jake Berg <jacobvictorberg@gmail.com>
2018-06-26 13:08:34 -0400

    added a basic database and get to call user

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
+const cors = require('cors');
+const { Client } = require('pg');
 
 app.use(express.json());
 app.use(cors());
 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   });
+
+const client = new Client({
+    database: 'social-media'
+});
+
+// route handlers go here
+app.get('/users', (req, res) => {
+    client.query('SELECT * FROM users', (err, result) => {
+        res.json(result.rows);
+    });
+});
 
 app.get("/", (req, res) => {
     res.json("cheesedingle")
