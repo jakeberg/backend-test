9bb63947167d29ec59961f865d0e975d4d191b52
Jake Berg <jacobvictorberg@gmail.com>
2018-06-26 15:57:28 -0400

    test

--- app.js
+++ app.js
@@ -17,6 +17,14 @@ app.get("/", (req, res) => {
     res.json("cheesedingle")
 });
 
+app.post('/users', (req,res) => {
+    const text = 'INSERT INTO users (username, bio) VALUES ($1, $2) RETURNING *';
+    const values = ['kenzie', 'Kenzie Academy is a user experience design and coding school in Indianapolis, Indiana. Our 6-month to 2-year program with 1-year paid apprenticeship is a new alternative to traditional colleges and short-term coding bootcamps.'];
+    client.query(text, values, (err, result) => {
+        res.send(result.rows)
+    });
+});
+
 app.listen( process.env.PORT || 3000, () => {
     console.log("this is working")
     client.connect((err) => {
