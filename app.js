213e05402f72e300788a2ff3a999640dd6e326cc
Jake Berg <jacobvictorberg@gmail.com>
2018-06-26 10:41:14 -0400

    took out was sending https from fetch on front end

--- app.js
+++ app.js
@@ -4,16 +4,11 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
 
-app.get("/data", (req, res) => {
-    res.end("cheesedingle")
+app.get("/", (req, res) => {
+    res.json("cheesedingle")
 });
 
-app.listen( process.env.PORT|| 3000, () => {
+app.listen( process.env.PORT || 3000, () => {
     console.log("this is working")
 })
