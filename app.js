4fe5e31acb1c6a31ccb832a78f8eefa6fffc07f9
Jake Berg <jacobvictorberg@gmail.com>
2018-06-27 13:26:58 -0400

    test

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
-// const client = new Client({
-//     host: 'ec2-54-83-60-13.compute-1.amazonaws.com',
-//     port: 5432,
-//     user: 'rusydaqqqjugzx',
-//     password: '6535fa0b8e78d2784f86fca4e503302f445f1f504d6f651ec4fbb64aef8274be',
-//   })
-
 const client = new Client({
-    database: "users"
-});
+    host: 'ec2-54-83-60-13.compute-1.amazonaws.com',
+    port: 5432,
+    user: 'rusydaqqqjugzx',
+    password: '6535fa0b8e78d2784f86fca4e503302f445f1f504d6f651ec4fbb64aef8274be',
+  })
+
+// const client = new Client({
+//     database: "users"
+// });
 
 app.get("/", (req, res) => {
     res.json("cheesedingle")
@@ -24,8 +24,8 @@ app.get("/", (req, res) => {
 app.post('/users', (req, res) => {
     console.log("Endpoint hit")
     console.log(req.body)
-    const text = 'INSERT INTO users (name, age, status) VALUES ($1, $2, $3) RETURNING *';
-    const values = ['jake', 29, "status test"];
+    const text = 'INSERT INTO users (username, bio) VALUES ($1, $2) RETURNING *';
+    const values = ['jake', "status test"];
     client.query(text, values, (err, result) => {
         console.log(result.rows[0]);
     });
