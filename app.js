ed9b3e77457d19c4c398b0bbaf97b921c0ae65a8
Jake Berg <jacobvictorberg@gmail.com>
2018-06-27 16:06:53 -0400

    test

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use(cors());
   const client = new Client({
     host: 'ec2-184-73-240-228.compute-1.amazonaws.com',
     user: 'ciwnpufebecnav',
+    port: '5432',
     password: '3b85f66a4ec80861f06c3944a3d4347242a3256d0e086a09af6b2889eb9af0e8',
     database: 'postgres://ciwnpufebecnav:3b85f66a4ec80861f06c3944a3d4347242a3256d0e086a09af6b2889eb9af0e8@ec2-184-73-240-228.compute-1.amazonaws.com:5432/d5d5ijhhn9te54'
   })
