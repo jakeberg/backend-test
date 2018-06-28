7a623bc62afb7d2bdc926f21e0f07be805c1b016
Jake Berg <jacobvictorberg@gmail.com>
2018-06-27 20:46:23 -0400

    made the /users endpoint into a get

--- app.js
+++ app.js
@@ -6,39 +6,17 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
-// const client = new Client({
-//     database: 'users'
-//   })
-
-//   const client = new Client({
-//     host: 'ec2-184-73-240-228.compute-1.amazonaws.com',
-//     user: 'ciwnpufebecnav',
-//     port: 5432,
-//     password: '3b85f66a4ec80861f06c3944a3d4347242a3256d0e086a09af6b2889eb9af0e8',
-//     database: 'postgres://ciwnpufebecnav:3b85f66a4ec80861f06c3944a3d4347242a3256d0e086a09af6b2889eb9af0e8@ec2-184-73-240-228.compute-1.amazonaws.com:5432/d5d5ijhhn9te54'
-//   })
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: true,
   });
 
-//   const client = new Client({
-//     Host: 'ec2-184-73-240-228.compute-1.amazonaws.com',
-//     Database: 'd5d5ijhhn9te54',
-//     User: 'ciwnpufebecnav',
-//     Port: 5432,
-//     Password: '3b85f66a4ec80861f06c3944a3d4347242a3256d0e086a09af6b2889eb9af0e8',
-//     URI: 'postgres://ciwnpufebecnav:3b85f66a4ec80861f06c3944a3d4347242a3256d0e086a09af6b2889eb9af0e8@ec2-184-73-240-228.compute-1.amazonaws.com:5432/d5d5ijhhn9te54',
-//     HerokuCLI: 'heroku pg:psql postgresql-cubic-50344 --app team-cheese-backend' 
-//   })
-
 app.get("/", (req, res) => {
     res.json("cheesedingle")
 });
 
-app.post('/users', (req, res) => {
-    console.log("Endpoint hit")
-    console.log(req.body)
+app.get('/users', (req, res) => {
+
     const text = 'INSERT INTO users (name, age, status) VALUES ($1, $2, $3) RETURNING *';
     const values = ['hello', 29, "status"];
     client.query(text, values, (err, result) => {
